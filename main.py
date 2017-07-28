e1d504efdcaec1f000761ef3975e31c57476f618
Aditya Kumar <aditya.kumar774@gmail.com>
2017-07-29 01:55:18 +0530

    Modifications

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 
-access_token = "1545666056.98346f2.38c9a48c189d43dd88ffe4b45bbd9d38&q"
 url = 'https://api.instagram.com/v1/users/search?q=abhishek_parmar.insta&access_token=1545666056.98346f2.38c9a48c189d43dd88ffe4b45bbd9d38'
-a = requests.get(url)
-print(a.json())
+user_details = requests.get(url)
+print(user_details.json())
