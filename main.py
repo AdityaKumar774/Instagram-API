82c3de2ebb36efe55b22cc3b9d385f9a0dcf536d
Aditya Kumar <aditya.kumar774@gmail.com>
2017-07-29 13:30:51 +0530

    Id of recent post is shown with username and userid

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import requests
 
-url = 'https://api.instagram.com/v1/users/search?q=abhishek_parmar.insta&access_token=1545666056.98346f2.38c9a48c189d43dd88ffe4b45bbd9d38'
+url = 'https://api.instagram.com/v1/users/1472346217/media/recent/?access_token=1545666056.98346f2.38c9a48c189d43dd88ffe4b45bbd9d38'
 user_details = requests.get(url)
-print(user_details.json())
+user_details = (user_details.json())
+# print (user_details)
+if user_details['meta']['code'] == 200:
+    user_name = user_details['data'][0]['user']['full_name']
+    print('User Name is: ' + user_name)
+    user_id = user_details['data'][0]['user']['id']
+    print('User Id is: ' + user_id)
+    media_id = user_details['data'][0]['id']
+    print ('Recent Post\'s id: ' + media_id)
+else:
+    print ('No User Found')
